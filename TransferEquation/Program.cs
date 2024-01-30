d4cf92b53753cd4fdb69f961662c86c5a6e31976
Серов Семен <sersemyon@gmail.com>
2024-01-30 22:52:36 +0300

    Добавьте файлы проекта.

---
+++ TransferEquation/Program.cs
@@ -0,0 +1,111 @@
+﻿using System.Numerics;
+
+namespace TransferEquation
+{
+    internal class Program
+    {
+        static double U_0 (double x, double X)
+        {
+            return Math.Sqrt(1 - x / X) * Math.Abs(Math.Sin(3 * x));
+        }
+
+        static double Mu(double t)
+        {
+            return 1.0 / (1.0 + t / 100) * Math.Pow(Math.Sin(t / 8), 2);
+        }
+
+        static double[] U(double a, double t, double[] X, double L, int n_x)
+        {
+            double[] Uex = new double[n_x];
+            for (int i = 0; i < n_x; i++)
+            {
+                if (a >= 0)
+                {
+                    if (X[i] >= a * t)
+                    {
+                        Uex[i] = U_0(X[i] - a * t, L);
+                    } 
+                    else
+                    {
+                        Uex[i] = Mu(t - X[i] / a);
+                    }
+                }
+                else
+                {
+                    if (L - X[i] >= Math.Abs(a) * t)
+                    {
+                        Uex[i] = U_0(L - X[i] - Math.Abs(a) * t, L);
+                    }
+                    else
+                    {
+                        Uex[i] = Mu(t - (L - X[i]) / Math.Abs(a));
+                    }
+                }
+            }
+            return Uex;
+        }
+
+        static void Main(string[] args)
+        {
+            int Nx = 41; // Количество точек
+            double L = 10;
+            double a = -1;
+            double Hx = L / (Nx - 1);
+            double Ht = Hx / Math.Abs(a);
+            double gm = a * Ht / Hx;
+            double t = 0;
+            double[] X = new double[Nx];
+            for (int i = 0; i < Nx; i++)
+            {
+                X[i] = i * Hx;
+            }
+            double[] Uj = new double[Nx];
+            if (a > 0)
+            {
+                for (int i = 0; i < Nx; i++)
+                {
+                    Uj[i] = U_0(X[i], L);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < Nx; i++)
+                {
+                    Uj[i] = U_0(X[Nx - i - 1], L);
+                }
+            }
+
+            double[] Uex = new double[1];
+            double[] Uj1 = new double[Nx];
+            while (t <= L)
+            {
+                Uj1[0] = Mu(t);
+                for (int i = 1; i < Nx; i++)
+                {
+                    if (a >= 0)
+                    {
+                        Uj1[i] = (1 - gm) * Uj[i] + gm * Uj[i - 1];
+                    }
+                    else
+                    {
+                        Uj1[i] = (1 + gm) * Uj[Nx - i - 1] - gm * Uj[Nx - i];
+                    }
+                }
+                for (int i = 0; i < Nx; i++)
+                {
+                    Uj[i] = Uj1[Nx - i - 1];
+                }
+                Console.WriteLine(Uj1.Length);
+                for (int i = 0; i < Uj1.Length; i++)
+                {
+                    Console.Write(Uj1[i] + ", ");
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+                t += Ht;
+            }
+            Uex = U(a, t - Ht, X, L, Nx);
+
+        }
+    }
+}
---
+++ TransferEquation/TransferEquationSolver.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransferEquation;
+
+class UnidirectionalTransferEquationSolver
+{
+    private double _h_t; // внешний шаг по времени
+    private double _h_tau; // внутренний шаг по времни
+    private double _h_x; // шаг по пространственной координате
+    private double[] _y; // дискретное множество текущих значений системы
+
+    public double H_t { 
+        get { return _h_t; }
+        set
+        {
+            _h_t = value;
+        }
+    } // Тут условие Куранта
+    public double H_x { get; }
+    public double InputValue { get; set; }
+    public double OutputValue 
+    {
+        get
+        {
+            return _y[^1];
+        }
+    }
+    
+    public UnidirectionalTransferEquationSolver(double[] initialValues, double h_t, double h_x)
+    {
+        _y = new double[initialValues.Length];
+        Array.Copy(initialValues, _y, initialValues.Length);
+        _h_t = h_t;
+        _h_x = h_x;
+    }
+
+    //public double Tick(double inputValue, double a)
+    //{
+    //    _h_tau = _h_x / Math.Abs(a);
+    //    int N = 1;
+    //    double lambda;
+    //    if (_h_tau >= _h_t)
+    //    {
+    //        _h_tau = _h_t;
+    //    } else
+    //    {
+    //        N = (int)Math.Ceiling(_h_t / _h_tau);
+    //        _h_tau = _h_t / N;
+    //    }
+    //    for (int t = 0; t < N; t++)
+    //    {
+    //        lambda = a * _h_tau / _h_x;
+    //        for (int i = _y.Length - 1; i > 0; i--)
+    //        {
+    //            _y[i] = (1.0 - lambda) * _y[i] + lambda * _y[i - 1];
+    //        }
+    //        _y[0] = (1.0 - lambda) * _y[0] + lambda * inputValue;
+    //    }
+    //}
+}
